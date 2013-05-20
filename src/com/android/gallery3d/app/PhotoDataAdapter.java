044fdc18d87d06958c2d37dee91ebe8650140694
jwan <jia.wan@intel.com>
2013-05-21 00:24:04 +0800

    Gallery3d: Fix the size is not updated timely
    BZ: 109188
    
    Since the mSize is the last album size, if last image is
    viewed, index is equal to mSize, so index could be minused
    by one, then the last two image could be showed when screen
    shot image is stored.
    
    Solution: remove the minus index, and update the items array
    to the current size.
    
    Change-Id: I9b1dbdd1e17f33dbadaa4204f06fed5a00b57210
    Signed-off-by: jwan <jia.wan@intel.com>
    Reviewed-on: http://android.intel.com:8080/108441
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Tested-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- src/com/android/gallery3d/app/PhotoDataAdapter.java
+++ src/com/android/gallery3d/app/PhotoDataAdapter.java
@@ -1041,7 +1041,8 @@ public class PhotoDataAdapter implements PhotoPage.Model {
                 }
                 if (!info.reloadContent) continue;
                 info.items = mSource.getMediaItem(
-                        info.contentStart, info.contentEnd);
+                        info.contentStart, Math.max(info.contentEnd,
+                        info.contentStart+info.size));
 
                 int index = MediaSet.INDEX_NOT_FOUND;
 
@@ -1081,9 +1082,9 @@ public class PhotoDataAdapter implements PhotoPage.Model {
                 }
 
                 // Don't change index if mSize == 0
-                if (mSize > 0) {
-                    if (index >= mSize) index = mSize - 1;
-                }
+                //if (mSize > 0) {
+                //    if (index >= mSize) index = mSize - 1;
+                //}
 
                 info.indexHint = index;
 
