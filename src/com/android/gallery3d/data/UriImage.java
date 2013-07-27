1df8d27762b833a443c5241730355a43f998f562
jwan <jia.wan@intel.com>
2013-07-27 18:18:00 +0800

    Use the right this reference to notify the waiter
    BZ: 75453
    
    The inner class use its own this reference to notify the waiter, but
    the waiter is watting on the outer class object reference, this lead to
    cancelling operation failure and cause anr.
    
    Feature: AOSP-Gallery
    Category: aosp improvement
    Severity: medium
    Origin: Internal
    Upstream-Candidate: Yes
    Orig-Change-Id: Ib0ba5c9f61d0b510effa0ca29f3b9f9ac0ca9d41
    Change-Id: I77068b9f05fe125e421df026afd90f12ff3eb09b
    Signed-off-by: wang, biao <biao.wang@intel.com>
    Reviewed-on: http://android.intel.com:8080/82592
    Reviewed-by: Wan, Jia <jia.wan@intel.com>
    Reviewed-by: Zhang, Hongyu <hongyu.zhang@intel.com>
    Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- src/com/android/gallery3d/data/UriImage.java
+++ src/com/android/gallery3d/data/UriImage.java
@@ -138,8 +138,8 @@ public class UriImage extends MediaItem {
         jc.setCancelListener(new CancelListener() {
             @Override
             public void onCancel() {
-                synchronized (this) {
-                    notifyAll();
+                synchronized (UriImage.this) {
+                    UriImage.this.notifyAll();
                 }
             }
         });
