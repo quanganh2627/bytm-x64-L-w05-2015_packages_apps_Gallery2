a125db053cd0f713eb6e116a4614b231f7aef5da
jwan <jia.wan@intel.com>
2013-08-20 09:17:19 +0800

    Gallery3d: Config a real edit id to filter
    BZ: 131557
    
    There is no editor whose id is imageShow, use
    imageZoom instead
    
    Change-Id: I45b06aed6072d1d286c73665d01828293c10c071
    Signed-off-by: jwan <jia.wan@intel.com>
    Reviewed-on: http://android.intel.com:8080/126280
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Tested-by: Shentu, JiazhenX <jiazhenx.shentu@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- src/com/android/gallery3d/filtershow/filters/ImageFilterSharpen.java
+++ src/com/android/gallery3d/filtershow/filters/ImageFilterSharpen.java
@@ -36,7 +36,7 @@ public class ImageFilterSharpen extends ImageFilterRS {
         representation.setTextId(R.string.sharpness);
         representation.setButtonId(R.id.sharpenButton);
         representation.setOverlayId(R.drawable.filtershow_button_colors_sharpen);
-        representation.setEditorId(R.id.imageShow);
+        representation.setEditorId(R.id.imageZoom);
         representation.setSupportsPartialRendering(true);
         return representation;
     }
