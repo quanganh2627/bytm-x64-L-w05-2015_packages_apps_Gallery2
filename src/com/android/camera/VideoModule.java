26320688a972ef21be290c942cb5b72424a9c843
Rukai Wei <rukaix.wei@intel.com>
2013-10-23 16:34:04 +0800

    [PORT FROM R43]Default Camera: don't startpreview again if has start recording
    BZ: 137930
    
    it failed and crash when startpreview the second time if recording quickly
    after just change to the VideoModule. because it has start recording and
    camera device has been locked.
    
    Change-Id: I031c4996369079a8f9b51dd19f23604a01246129
    Orig-Change-Id: Ia1dd3c229c078b53f7918926f040b92a6be17c2c
    Signed-off-by: Rukai Wei <rukaix.wei@intel.com>
    Reviewed-on: http://android.intel.com:8080/140221
    Reviewed-by: Vtminen, LassiX <lassix.vtminen@intel.com>
    Reviewed-by: Gong, Shuguang <shuguang.gong@intel.com>
    Reviewed-by: Koski, Anttu <anttu.koski@intel.com>
    Tested-by: Koski, Anttu <anttu.koski@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -607,7 +607,7 @@ public class VideoModule implements CameraModule,
 
     @Override
     public void onShutterButtonClick() {
-        if (mUI.collapseCameraControls() || mSwitchingCamera || mActivity.mCameraDevice == null) return;
+        if (mUI.collapseCameraControls() || mSwitchingCamera || mActivity.mCameraDevice == null || !mPreviewing) return;
 
         boolean stop = mMediaRecorderRecording;
 
@@ -775,7 +775,7 @@ public class VideoModule implements CameraModule,
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    startPreview();
+                    if (!mMediaRecorderRecording && !mPreviewing) startPreview();
                 }
             }).start();
         } else {
