ea784037e5272fbddf960bf1e8ec4fdbc3018c43
Muthukumar K <muthukumar.kandasamy@intel.com>
2013-11-26 16:35:27 +0530

    [DolbyDS1] Code refactor of Gallery, Music and AudioService for DS1 changes
    BZ: 146038
    
    This patch has the refactored Gallery2 code changes for DS1.
    
    Change-Id: I43e7d37e5dedd1a98b52f5616d89d6bb8a2b6950
    Category: feature differentiation
    Domain: AUDIO_MEDIA-Dolby
    Origin: 3rd-party Dolby
    Upstream-Candidate: no, proprietary
    Signed-off-by: charitardha <satya.charitardha.jayanti@intel.com>

---
+++ src/com/android/gallery3d/app/DsClientOverlayHelper.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This file was modified by Dolby Laboratories, Inc. The portions of the
+ * code that are surrounded by "DOLBY..." are copyrighted and
+ * licensed separately, as follows:
+ *
+ * (C) 2011-2012 Dolby Laboratories, Inc.
+ * All rights reserved.
+ *
+ * This program is protected under international and U.S. Copyright laws as
+ * an unpublished work. This program is confidential and proprietary to the
+ * copyright owners. Reproduction or disclosure, in whole or in part, or the
+ * production of derivative works therefrom without the express permission of
+ * the copyright owners is prohibited.
+ *
+ */
+
+//DOLBY_DAP_GUI
+package com.android.gallery3d.app;
+
+import android.content.Context;
+import android.widget.ToggleButton;
+
+public class DsClientOverlayHelper {
+    private ToggleButton mSwitch;
+
+    public DsClientOverlayHelper(Context context) {
+    }
+
+    public ToggleButton getTB() {
+        return mSwitch;
+    }
+    public int[] layoutDolbySwitch() {
+        int[] dim = new int[4];
+        return dim;
+    }
+}
+//DOLBY_DAP_GUI END
